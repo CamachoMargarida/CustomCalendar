6cbd832874c25f9b1ab1709f902a555de34b5140
Margarida Camacho <margarida.camacho@axians.com>
2024-11-21 17:50:06 +0000

    Revert "init of Event"
    This reverts commit 8ba9e6ccf20d69c95d4e53f27d15c804906d5358.

--- Sources/CustomCalendar/Settings/Event.swift
+++ Sources/CustomCalendar/Settings/Event.swift
@@ -10,10 +10,4 @@ public struct Event: Identifiable, Equatable {
     public var id: Int
     var title: String
     var date: Date
-    
-    public init(id: Int, title: String, date: Date) {
-        self.id = id
-        self.title = title
-        self.date = date
-    }
 }
