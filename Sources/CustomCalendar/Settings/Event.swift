a7dc39b460dd27fcf42e8b0bb5d2dcb70dcad6e9
Margarida Camacho <margarida.camacho@axians.com>
2024-11-21 17:58:07 +0000

    Change event struct

--- Sources/CustomCalendar/Settings/Event.swift
+++ Sources/CustomCalendar/Settings/Event.swift
@@ -7,7 +7,13 @@
 import SwiftUI
 
 public struct Event: Identifiable, Equatable {
-    public var id: Int
-    var title: String
-    var date: Date
+    public var id: Int?
+    var title: String?
+    var date: Date?
+    
+    init(id: Int? = nil, title: String? = nil, date: Date? = nil) {
+        self.id = id
+        self.title = title
+        self.date = date
+    }
 }
