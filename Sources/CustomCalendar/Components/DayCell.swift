231ed173de84bfe95d921cd69f4cfab586e705e3
Margarida Camacho <margarida.camacho@axians.com>
2024-11-22 21:36:27 +0000

    Fix day cell layout

--- Sources/CustomCalendar/Components/DayCell.swift
+++ Sources/CustomCalendar/Components/DayCell.swift
@@ -30,17 +30,17 @@ struct DayCell: View {
                     .background(calendarDate.getBackColor())
                 
                 VStack(alignment: .center, spacing: 2) {
-                    ForEach(calendarDate.events.indices.prefix(4), id: \.self) { index in
+                    ForEach(calendarDate.events.indices.prefix(3), id: \.self) { index in
                         Text(calendarDate.events[index])
-                            .frame(maxWidth: .infinity, maxHeight: calendarDate.events.count < 4 ? .infinity : nil)
+                            .frame(maxWidth: .infinity, maxHeight: .infinity)
                             .foregroundStyle(calendarDate.manager.colors.eventTextColor)
                             .font(Fonts(customSize: 10).regularTextFont)
                             .background(calendarDate.manager.colors.eventBackColor)
                             .clipShape(RoundedRectangle(cornerRadius: 2))
                     }
                     
-                    if calendarDate.events.count > 4 {
-                        Text("+\(calendarDate.events.count - 4)")
+                    if calendarDate.events.count > 3 {
+                        Text("+\(calendarDate.events.count - 3)")
                             .frame(maxWidth: .infinity, maxHeight: calendarDate.events.count == 1 ? .infinity : nil)
                             .foregroundStyle(calendarDate.manager.colors.eventTextColor)
                             .font(Fonts(customSize: 10).regularTextFont)
@@ -51,7 +51,7 @@ struct DayCell: View {
                 .frame(maxHeight: .infinity, alignment: .bottom)
                 .padding(2)
             }
-            .frame(maxWidth: cellSize * 2, maxHeight: cellSize * 3)
+            .frame(width: cellSize * 2, height: cellSize * 2.5)
         }
     }
 }
