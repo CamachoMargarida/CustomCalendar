5994569eca778dd64bed6f519e923ca7260ed269
Margarida Camacho <margarida.camacho@axians.com>
2024-12-13 12:22:53 +0000

    Revert "Test"
    This reverts commit c68dc4a0985e3167c3e3f3919c71383a12587aa0.

--- Sources/CustomCalendar/CustomCalendar.swift
+++ Sources/CustomCalendar/CustomCalendar.swift
@@ -57,15 +57,6 @@ public struct CustomCalendar: View {
             }
             .onChange(of: manager.selectedDates) { newList in
                 selectedDates = newList
-                
-                if selectedDates.count > 1 {
-                    manager.endDate = selectedDates.last
-                }
-                
-                else {
-                    manager.startDate = selectedDates.first
-                    manager.endDate = nil
-                }
             }
         }
         .background(manager.colors.backgroundColor)
